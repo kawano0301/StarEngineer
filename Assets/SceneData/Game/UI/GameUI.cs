dff8b02b052e3d8375bc42a30e882c77562af558
5221051 <5221051@st.hsc.ac.jp>
2024-12-31 02:40:23 +0900

    タイマー完成(コードに品質問題あり)

--- Assets/SceneData/Game/UI/GameUI.cs
+++ Assets/SceneData/Game/UI/GameUI.cs
@@ -7,12 +7,7 @@ namespace Game.UI
 {
     public class GameUI : MonoBehaviour
     {
-        //[SerializeField] UIScriptable;
-        Sprite[] m_timerSprite;
-        Image m_timerLeft;
-        Image m_timerRight;
-
-        float m_gameTimer;//Scriptable‚Åˆµ‚¤’l‚ðŠî‚É‚·‚é
+        float m_gameTimer;
 
         void Start()
         {
@@ -23,24 +18,7 @@ namespace Game.UI
         void Update()
         {
             m_gameTimer -= Time.deltaTime;
-            TimerSpriteUpdate(m_gameTimer);
         }
 
-        void TimerSpriteUpdate(float timer)
-        {
-            //m_timerLeft.sprite = m_timerSprite[];
-            //m_timerRight.sprite = m_timerSprite[];
-
-        }
-/*
-        int OneDigit(float timer)
-        {
-            return;
-        }
-        int TwoDigit(float timer)
-        {
-            return;
-        }
-*/
     }
 }
